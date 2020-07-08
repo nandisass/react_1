19a486fe3ef3420906de5d665c9ce770aff81d8f
iamsaswata <nandi.pritam007@gmail.com>
2020-07-08 12:12:03 +0530

    Updates

---
+++ precache-manifest.43670621339166cc581466d9aeb48f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f72dc3af92517f3ac66d0d18572b25e",
+    "url": "/react_1/index.html"
+  },
+  {
+    "revision": "31961eabd8cebd25cbc9",
+    "url": "/react_1/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b82f89c6f5c294a255b3",
+    "url": "/react_1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "31961eabd8cebd25cbc9",
+    "url": "/react_1/static/js/2.51b3c565.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/react_1/static/js/2.51b3c565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82f89c6f5c294a255b3",
+    "url": "/react_1/static/js/main.336210ff.chunk.js"
+  },
+  {
+    "revision": "dd0abecf5ef03f96ce47",
+    "url": "/react_1/static/js/runtime-main.0a0cd30f.js"
+  }
+]);
